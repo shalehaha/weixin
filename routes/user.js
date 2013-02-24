a316aa9ea4938b92fb9ccc193890b73987b32a13
jgeng <jgeng@adobe.com>
2013-02-24 23:24:44 +0800

    add verify

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,26 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+
+exports.verify = function(req, res){
+  
+    console.log(req.query.signature);
+    console.log(req.query.timestamp);
+    console.log(req.query.nonce);
+        console.log(req.query.echostr);
+
+
+    
+
+  res.send("respond with a resource");
+};
+
+
+exports.message = function(req, res){
+  
+    console.log(req.query.signature);
+    console.log(req.query.timestamp);
+    console.log(req.query.nonce);
+    
+};
